4b4d9c74095346f04be94c2ca11add98f5a47955
BrendaDiaz <yarelidiaz@uas.edu.mx>
2023-12-20 12:08:14 -0700

    5 colocando boton de cambio

--- Fase1/script.js
+++ Fase1/script.js
@@ -14,3 +14,54 @@ function removeActiveClasses(){
         panel.classList.remove('active');
     })
 }
+
+//modulo de avance o progreso
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+//const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > panels.length) {
+        currentActive = panels.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    panels.forEach((panel, idx) => {
+        if(idx < currentActive) {
+            panel.classList.add('active')
+        } else {
+            panel.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (panels.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === panels.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
